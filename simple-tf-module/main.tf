926746fd6170ff0746205c36c5ebfd69c9f7895f
yakirlevi <yakir.l@quali.com>
2022-04-05 15:16:04 +0300

    initial commit

---
+++ simple-tf-module/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 0.13"
+//  backend "local" {
+//    path    = "mybackend/terraform.tfstate"
+//  }
+}
+
+// Shell
+resource "null_resource" "task_log" {
+  provisioner "local-exec" {
+    command = "echo task_log. variables: rabbit_endpoint=bla."
+  }
+}
+
+resource "null_resource" "task_long" {
+  provisioner "local-exec" {
+    command = "echo \"long task start\" && sleep 5 && echo \"long task end\""
+  }
+}
