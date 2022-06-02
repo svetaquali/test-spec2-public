9666137b378998e3769d9929c1a0606735ef9a55
Liav A <liavadato@gmail.com>
2022-06-02 21:06:38 +0300

    Update variables.tf

--- terraform/simple-tf-module/variables.tf
+++ terraform/simple-tf-module/variables.tf
@@ -4,6 +4,9 @@ variable "test_input1" {
 }
 // here zach! I made a change!
 // And even a multi liner!
+// I make it bigger
+// so it will be easier
+// to notice
 
 variable "test_input2" {
     type = string
