664874a9ac972f043c9029559a1ead9081071389
Liav A <liavadato@gmail.com>
2022-06-02 20:43:06 +0300

    Update variables.tf

--- terraform/simple-tf-module/variables.tf
+++ terraform/simple-tf-module/variables.tf
@@ -2,7 +2,8 @@ variable "test_input1" {
     type = string
     default = "default1"
 }
-// test asfasasf
+// here zach! I made a change!
+// And even a multi liner!
 
 variable "test_input2" {
     type = string
