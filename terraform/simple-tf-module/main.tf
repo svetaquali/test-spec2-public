745d0d5cab5c0e8d8cedcfeac9061d8f86a57a55
Liav A <liavadato@gmail.com>
2022-08-03 17:13:15 +0300

    Update main.tf

--- terraform/simple-tf-module/main.tf
+++ terraform/simple-tf-module/main.tf
@@ -4,7 +4,7 @@ terraform {
 //    path    = "mybackend/terraform.tfstate"
 //  }
 }
-// 
+// Torque can now update environments!!!
 
 resource "null_resource" "task_log" {
   provisioner "local-exec" {
