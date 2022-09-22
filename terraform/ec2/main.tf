ef82d0aeae1b5245066d1c9d3b741605c68e8757
alexander.g <alexander.g@qualisystems.com>
2022-09-22 17:28:48 +0300

    more ec2 examples

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -19,15 +19,14 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
 
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
