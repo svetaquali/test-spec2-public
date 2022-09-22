0baf5dd2f4e029d01c2e911df3a216f0a2d5f0b8
alexander.g <alexander.g@qualisystems.com>
2022-09-22 17:54:58 +0300

    zone fix2

--- terraform/ec2-example/main.tf
+++ terraform/ec2-example/main.tf
@@ -18,7 +18,6 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-  availability_zone = "eu-west-1"
 
   tags = {
     Name = "tf-example"
