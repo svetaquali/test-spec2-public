6089058e3195be84c7e6bc2fe42e07ec50264070
tomer-c <tomer.c@quali.com>
2022-11-30 13:13:52 +0200

    back to id

--- terraform/s3-tomerc/main.tf
+++ terraform/s3-tomerc/main.tf
@@ -71,3 +71,7 @@ output "s3_bucket_arn" {
 output "s3_bucket_regional_domain_name" {
   value = aws_s3_bucket.bucket.bucket_regional_domain_name
 }
+
+output "s3_bucket_id" {
+  value = aws_s3_bucket.bucket.id
+}
