b5b6c26bb7d340e627eb5eb4d70f575369fdbbb1
shirel-m <77104842+shirel-m@users.noreply.github.com>
2022-12-12 14:26:16 +0200

    added a create ec2 blueprint

---
+++ terraform/create-ec2-instance/variables.tf
@@ -0,0 +1,18 @@
+variable "instance_type" {
+  description = "(Optional) The instance type to use for the instance. defaults to t3.micro."
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami" {
+  description = "(Optional) AMI to use for the instance. Defaults to latest Ubuntu 18.04 LTS."
+  type        = string
+  default     = ""
+}
+
+# variable "access_key" {
+# }
+# variable "secret_key" {
+# }
+# variable "token" {
+# }
