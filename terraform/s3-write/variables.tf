ebb8d2936a755d6536dee8b653ed8188cda4cca2
yakirlevi <78429967+yakirlevi@users.noreply.github.com>
2023-06-08 12:40:38 +0300

    Update variables.tf

--- terraform/s3-write/variables.tf
+++ terraform/s3-write/variables.tf
@@ -1,3 +1,9 @@
 variable "bucket_name" {}
 variable "object_key" {}
 variable "content" {}
+
+variable "region" {
+    description = "Region where to create resources" 
+    type = string
+    default = "eu-west-1"
+}
