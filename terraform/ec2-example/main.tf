1fd2cd38edec56d40c373cb639fb6d7c89785fad
yakirlevi <78429967+yakirlevi@users.noreply.github.com>
2023-06-26 10:42:25 +0300

    Update main.tf

--- terraform/ec2-example/main.tf
+++ terraform/ec2-example/main.tf
@@ -18,13 +18,13 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "172.16.10.0/24"
-
+  not-exist = "sd"
   tags = {
     Name = "tf-example"
   }
 }
 
-abc
+
 resource "aws_network_interface" "foo" {
   subnet_id   = aws_subnet.my_subnet.id
   private_ips = ["172.16.10.100"]
