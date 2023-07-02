36594593af7be503f3803d68284888a718e58fe3
Amir Omazgin <amir.o@quali.com>
2023-07-02 13:24:59 +0300

    added s3 amir

---
+++ terraform/s3-write-amir/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-east-1"
+}
+
+locals {
+  common_tags = {
+    creator        = "terraform"
+  }
+  
+  initiator = "probably torque"
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket  = "${var.bucket_name}"
+  key     = "${var.object_key}.json"
+  #content = "{ \"Content\": \"${var.content}\"}"
+  content = <<EOF
+Content: ${var.content}
+EOF
+  
+  tags = merge(
+    local.common_tags,
+    {
+      "custom-tag-2" = local.initiator
+    }
+  )
+}
