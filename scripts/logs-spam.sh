978dc4dabc208315d6a3f22f271cb701625226b9
yakirlevi <78429967+yakirlevi@users.noreply.github.com>
2024-01-08 15:07:28 +0200

    Update logs-spam.sh

--- scripts/logs-spam.sh
+++ scripts/logs-spam.sh
@@ -4,9 +4,9 @@ echo "spamming logs"
 
 for i in {1..2000000}; do
   timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-  random_log="Log message $i. Just veryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy superrrrrrrrrrrrrrrrrrrrrrrr   longgggggggggggggggggggggggggggggggggg randommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm text"
+  random_log="Log message $i. Just veryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy superrrrrrrrrrrrrrrrrrrrrrrr   longgggggggggggggggggggggggggggggggggg randommmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm texttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt"
   echo "[$timestamp] $random_log"
-  sleep 0.$(shuf -i 1-10 -n 1)
+  sleep 0.0$(shuf -i 1-10 -n 1)
 done
 
 echo "ended"
