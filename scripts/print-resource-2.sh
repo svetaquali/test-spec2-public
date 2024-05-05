3d6df887acb837358d8b1a921422df74fae245c7
yakirlevi <78429967+yakirlevi@users.noreply.github.com>
2024-05-05 08:50:06 +0300

    Update print-resource-2.sh

--- scripts/print-resource-2.sh
+++ scripts/print-resource-2.sh
@@ -6,4 +6,8 @@ contract_path=$CONTRACT_FILE_PATH
 # echo "extracting power state attribute from resource"
 # will print the attribute "power_state" of the resource
 state=$(jq --arg ResourceId "$resource_id" --arg GrainPath "$grain_path" '.resources[] | select(.identifier == $ResourceId and .grain_path == $GrainPath) | .attributes | .power_state' $contract_path)
-echo -n $state | tr -d '"'
+
+state_tr=`echo -n $state | tr -d '"'`
+
+echo $state_tr
+export $state_tr
