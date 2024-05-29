3d07e09c817a8f8d8f62d6632d98960f7ccffbb8
yakirlevi <78429967+yakirlevi@users.noreply.github.com>
2024-05-29 08:58:45 +0300

    Create main.tf

---
+++ terraform/s3-create-bucket/main.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.name
+  force_destroy = true
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    A_NEW_TAG = "NEW TAG"
+  }
+}
