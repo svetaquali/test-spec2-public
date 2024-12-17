6eb409add1928a9d4ddd06eadee518f4ce61280f
shirel.m <shirel.m@quali.com>
2024-12-17 12:26:37 +0200

    changed service account name

--- terraform/gcp-compute-service/main.tf
+++ terraform/gcp-compute-service/main.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_service_account" "default" {
-  account_id   = "compute-engine-default-service-account"
+  account_id   = "compute-engine-sa"
   display_name = "Custom SA for VM Instance"
 }
 
