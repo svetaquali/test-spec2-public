3da88092947d40f1e6ff6a2c91603e77dcdbaf85
shirel.m <shirel.m@quali.com>
2025-01-21 14:14:35 +0200

    fixed app

--- cdk/cdk-multi-stack-app/app.py
+++ cdk/cdk-multi-stack-app/app.py
@@ -5,9 +5,9 @@ from cdk_multi_stack_app.s3_stack import S3Stack
 app = App()
 
 # Create the EC2 stack
-ec2_stack = Ec2Stack(app, "Ec2Stack")
+ec2_stack = Ec2Stack(app, "Ec2Stack", env=cdk.Environment(account='046086677675', region='eu-west-1'))
 
 # Create the S3 stack
-s3_stack = S3Stack(app, "S3Stack")
+s3_stack = S3Stack(app, "S3Stack", env=cdk.Environment(account='046086677675', region='eu-west-1'))
 
 app.synth()
