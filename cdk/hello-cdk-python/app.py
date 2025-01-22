79fbc630d349fcbc9cd864e62a577e7f3ff52d33
shirel.m <shirel.m@quali.com>
2025-01-22 15:40:40 +0200

    fixed

--- cdk/hello-cdk-python/app.py
+++ cdk/hello-cdk-python/app.py
@@ -3,11 +3,27 @@ import os
 
 import aws_cdk as cdk
 
-from hello_cdk_python import HelloCdkStack
+from hello_cdk_python.hello_cdk_python_stack import HelloCdkPythonStack
+
 
 app = cdk.App()
-HelloCdkStack(app, "HelloCdkStack",
-              env=cdk.Environment(account='046086677675', region='eu-west-1'),
-              )
+HelloCdkPythonStack(app, "HelloCdkPythonStack",
+                    env=cdk.Environment(account='046086677675', region='eu-west-1')
+    # If you don't specify 'env', this stack will be environment-agnostic.
+    # Account/Region-dependent features and context lookups will not work,
+    # but a single synthesized template can be deployed anywhere.
+
+    # Uncomment the next line to specialize this stack for the AWS Account
+    # and Region that are implied by the current CLI configuration.
+
+    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+    # Uncomment the next line if you know exactly what Account and Region you
+    # want to deploy the stack to. */
+
+    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    )
 
 app.synth()
---
+++ cdk/hello-cdk-python/hello_cdk_python/hello_cdk_python_stack.py
@@ -0,0 +1,35 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_lambda as _lambda
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class HelloCdkPythonStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        my_function = _lambda.Function(
+            self, "HelloWorldFunction",
+            runtime=_lambda.Runtime.NODEJS_20_X,  # Provide any supported Node.js runtime
+            handler="index.handler",
+            code=_lambda.Code.from_inline(
+                """
+                exports.handler = async function(event) {
+                  return {
+                    statusCode: 200,
+                    body: JSON.stringify('Hello World!'),
+                  };
+                };
+                """
+            ),
+        )
+
+        my_function_url = my_function.add_function_url(
+            auth_type=_lambda.FunctionUrlAuthType.NONE,
+        )
+
+        # Define a CloudFormation output for your URL
+        CfnOutput(self, "myFunctionUrlOutput", value=my_function_url.url)
