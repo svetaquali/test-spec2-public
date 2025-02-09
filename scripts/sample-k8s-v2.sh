ff4e77dfd813b87652e66d8ceb14c7014eac3342
yakirlevi <yakir.l@quali.com>
2025-02-09 11:52:46 +0200

    added k8s script

---
+++ scripts/sample-k8s-v2.sh
@@ -0,0 +1,5 @@
+export testoutput="this is first value"
+export testoutput2="this is second value"
+export testoutput3="arg is ${1}"
+
+echo "export testoutput2=\"this is second value\"" > ~/torque-envs.sh
